edd2b7260de64cefa38ec40f3b728d0ca1fb888c
Praveen Kumar K <125640021+shinchan2627@users.noreply.github.com>
2023-03-12 17:43:02 +0530

    Hangman game newly try using python
    refer by google, python blog

---
+++ hangman_code.py
@@ -0,0 +1,113 @@
+import random
+
+hangman = [  
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+
+                r"""
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+
+                r"""
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+
+                r"""
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+
+                r"""
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \
+                   -
+                """
+    ]
+
+words = ["python", "oracle", "java", "proffessor","berlin","rio","tokyo","denver","france","hitler"]
+word = random.choice(words)
+
+def StartGame(word):
+    guess_word = [ "_" for i in word ]
+    tries = 0
+    print(hangman[tries])
+    print("Guess a word : {}".format(" ".join(guess_word)))
+
+    while True:
+        user = input("Guess a Letter [a-z] : ")
+        if not user.isalpha():
+            print("Please enter [a-z] only.")
+            continue
+        if user in word:
+            indexes = [ i for i in range(len(word)) if word[i] == user ]
+            for index in indexes:
+                guess_word[index] = user
+            print("=" * 50)
+            print("Good : {}".format("".join(guess_word)))
+
+            if "".join(guess_word) == word:
+                return "Wow! You Guessed :)"
+        else:
+            tries = tries + 1
+            print(hangman[tries])
+            if tries == 6:
+                return "Sorry! Game Over."
+            print("Sorry! Try Again : {}".format("".join(guess_word)))
+
+if __name__ == "__main__":
+    while True:
+        print(StartGame(word))
+        play_again = input("Do You Want Play Again [y/n] : ")
+        if play_again == "y":
+            word = random.choice(words)
+            continue
+        else:
+            break
